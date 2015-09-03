c9bc825f71f2223b30c55366ddcfba1f4d671fc6
kostyantyn.drobyazko <kostyantyn.drobyazko@gmail.com>
2015-09-03 15:48:58 +0300

    strategy pattern review

---
+++ patters-revision/src/main/java/com/epam/pdp/patterns/behavioral/strategy/AxeStragegy.java
@@ -0,0 +1,8 @@
+package com.epam.pdp.patterns.behavioral.strategy;
+
+public class AxeStragegy implements FightStrategy {
+    public void fight() {
+        System.out.println("Fight with axe");
+    }
+
+}
