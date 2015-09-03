c9984667349c57ea912f9f8c273613ddc57e87b8
kostyantyn.drobyazko <kostyantyn.drobyazko@gmail.com>
2015-09-03 16:41:54 +0300

    abstract factory review

---
+++ patters-revision/src/main/java/com/epam/pdp/patterns/creational/abstractfactory/products/Button.java
@@ -0,0 +1,5 @@
+package com.epam.pdp.patterns.creational.abstractfactory.products;
+
+public interface Button {
+    void draw();
+}
---
+++ patters-revision/src/main/java/com/epam/pdp/patterns/creational/abstractfactory/products/XwindowWindow.java
@@ -0,0 +1,7 @@
+package com.epam.pdp.patterns.creational.abstractfactory.products;
+
+public class XwindowWindow implements Window {
+    public void draw() {
+    }
+
+}
