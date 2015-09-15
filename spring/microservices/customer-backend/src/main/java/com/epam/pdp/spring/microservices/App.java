dbe61f8dea5e9e4ab65bf0bcdc22da38c084708f
kostyantyn.drobyazko <kostyantyn.drobyazko@gmail.com>
2015-09-15 12:54:28 +0300

    microservices

---
+++ spring/microservices/customer-backend/src/main/java/com/epam/pdp/spring/microservices/App.java
@@ -0,0 +1,10 @@
+package com.epam.pdp.spring.microservices;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
